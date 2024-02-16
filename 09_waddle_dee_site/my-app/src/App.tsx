6bb94c7b25560b18fafcc25f835089abb5f6697c
Avery Cheng <AveryZC14@gmail.com>
2024-02-17 00:07:59 +1100

    made some progress

---
+++ 09_waddle_dee_site/my-app/src/App.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <header className='main-header'>
+      <h1 className='site-title'>Waddle Dee Site</h1>
+      <nav className='site-nav'>
+        hello
+        <ul className='nav-list'>
+          <li className="nav-item">
+            <a href="google.com" className="nav-link">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="google.com" className="nav-link">About</a>
+          </li>
+          <li className="nav-item">
+            <a href="google.com" className="nav-link">Waddle Dees</a>
+          </li>
+          <li className="nav-item">
+            <a href="google.com" className="nav-link">Deeterbase</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main className='main-app'>
+      <div className="section">
+        lkjasdf
+      </div>
+      <div className="section">
+
+      </div>
+      <div className="section">
+
+      </div>
+    </main>
+    <footer>
+      <h2>
+        links
+      </h2>
+      <ul>
+        <li className="footer_li"><a href="https://google.com" className="footer_a">me</a></li>
+        <li className="footer_li"><a href="https://google.com" className="footer_a">a</a></li>
+        <li className="footer_li"><a href="https://google.com" className="footer_a">b</a></li>
+        <li className="footer_li"><a href="https://google.com" className="footer_a">c</a></li>
+      </ul>
+    </footer>
+    </>
+  );
+}
+
+export default App;
