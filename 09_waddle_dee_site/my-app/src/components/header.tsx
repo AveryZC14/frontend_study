9f8ea2bbe37b96d1f8e7d12ec449b26121137f82
Avery Cheng <AveryZC14@gmail.com>
2024-02-23 01:20:45 +1100

    it works

--- 09_waddle_dee_site/my-app/src/components/header.tsx
+++ 09_waddle_dee_site/my-app/src/components/header.tsx
@@ -1,37 +1,35 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { HeaderNavLink } from "./HeaderNavLink";
 
-export function Header(){
-    interface page{
-        path:string;
-        label?:string;
-    }
-    const pages:Array<page> = [{path:"Home"},{path:"About"},{path:"WaddleDees", label:"Waddle Dees"}];
-    return (
-        <>
-        <header className='main-header'>
+export function Header() {
+  interface page {
+    path: string;
+    label?: string;
+  }
+  const pages: Array<page> = [
+    { path: "Home" },
+    { path: "About" },
+    { path: "WaddleDees", label: "Waddle Dees" }
+  ];
+  return (
+    <>
+      <header className="main-header">
         <Link to="/Home" className="title-link">
-            <h1 className='site-title'>Waddle Dee Site</h1>
-            </Link>
-            <nav className='site-nav'>
-                <ul className='nav-list'>
-                    {pages.map((linkPath:{path:string,label?:string})=>{return(<HeaderNavLink linkPath = {linkPath.path} linkLabel = {linkPath.label}/>)})}
-                    
-                {/* <li className="nav-item">
-                    <Link to="/Home" className="nav-link">Home</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/About">About</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/WaddleDees">Waddle Dees</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/Deeterbase">Deeterbase</Link>
-                </li> */}
-                </ul>
-            </nav>
-            </header>
-        </>
-    )
+          <h1 className="site-title">Waddle Dee Site</h1>
+        </Link>
+        <nav className="site-nav">
+          <ul className="nav-list">
+            {pages.map((linkPath: { path: string; label?: string }) => {
+              return (
+                <HeaderNavLink
+                  linkPath={linkPath.path}
+                  linkLabel={linkPath.label}
+                />
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
