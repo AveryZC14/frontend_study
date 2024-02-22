ef1be0203ab9733276a62bdeeb3788b62c29d191
Avery Cheng <AveryZC14@gmail.com>
2024-02-23 00:25:34 +1100

    header with links

--- 09_waddle_dee_site/my-app/src/components/header.tsx
+++ 09_waddle_dee_site/my-app/src/components/header.tsx
@@ -1,23 +1,26 @@
-export function header(){
+import { Link } from "react-router-dom"
+
+export function Header(){
+    // const pages = ["Home","About"];
     return (
         <>
         <header className='main-header'>
-            <a href="/Home">
+        <Link to="/Home" className="title-link">
             <h1 className='site-title'>Waddle Dee Site</h1>
-            </a>
+            </Link>
             <nav className='site-nav'>
                 <ul className='nav-list'>
                 <li className="nav-item">
-                    <a href="/" className="nav-link">Home</a>
+                    <Link to="/Home" className="nav-link">Home</Link>
                 </li>
                 <li className="nav-item">
-                    <a href="google.com" className="nav-link">About</a>
+                    <Link className="nav-link" to="/About">About</Link>
                 </li>
                 <li className="nav-item">
-                    <a href="google.com" className="nav-link">Waddle Dees</a>
+                    <Link className="nav-link" to="/WaddleDees">Waddle Dees</Link>
                 </li>
                 <li className="nav-item">
-                    <a href="google.com" className="nav-link">Deeterbase</a>
+                    <Link className="nav-link" to="/Deeterbase">Deeterbase</Link>
                 </li>
                 </ul>
             </nav>
--- 09_waddle_dee_site/my-app/src/pages/Root.tsx
+++ 09_waddle_dee_site/my-app/src/pages/Root.tsx
@@ -1,29 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet , Link } from "react-router-dom";
+import { Header } from "../components/Header";
 
 export function Root(){
     return (
         <>
-        <header className='main-header'>
-        <a href="/Home" className="title-link">
-            <h1 className='site-title'>Waddle Dee Site</h1>
-            </a>
-            <nav className='site-nav'>
-                <ul className='nav-list'>
-                <li className="nav-item">
-                    <a href="/Home" className="nav-link">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a href="/About" className="nav-link">About</a>
-                </li>
-                <li className="nav-item">
-                    <a href="google.com" className="nav-link">Waddle Dees</a>
-                </li>
-                <li className="nav-item">
-                    <a href="google.com" className="nav-link">Deeterbase</a>
-                </li>
-                </ul>
-            </nav>
-            </header>
+            <Header/>
             <main className='main-app'>
                 <div id="detail">
                     <Outlet />
